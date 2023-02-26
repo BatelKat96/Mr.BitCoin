54220162bad4a8d57a1996e639999dbf1113bc84
BatelKat96 <batelkat@gmail.com>
2023-02-26 13:24:39 +0200

    Fix header and footer design

--- src/App.vue
+++ src/App.vue
@@ -22,24 +22,4 @@ export default {
 </script>
 
 <style scoped>
-main {
-  min-height: calc(80.5vh);
-}
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 </style>
